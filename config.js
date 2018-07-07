dab1c1009b8aafa94a51d51d14e039e7d650aacd
Thomas Whitaker <thomaswhitaker@protonmail.com>
2018-07-07 23:43:42 +0100

    7/7/18
    Not Tom's

--- config.js
+++ config.js
@@ -1,15 +1,15 @@
 module.exports = {
-  title: 'Dev Blog', // Required
-  author: 'Ryan Fitzgerald', // Required
-  tagline: 'Full-stack Web Developer',
+  title: 'tomWritesCode', // Required
+  author: 'Thomas Whitaker', // Required
+  tagline: 'I Google a lot of stuff to build websites',
   primaryColor: '#3498db', // Required
   showHeaderImage: true,
   showShareButtons: true,
   postsPerPage: 5, // Required
   social: {
-    website: 'https://ryanfitzgerald.ca',
-    github: 'https://github.com/ryanfitzgerald',
-    twitter: 'https://twitter.com/ryanafitzgerald',
-    linkedin: 'https://ca.linkedin.com/in/ryanafitzgerald'
-  }
+    website: 'http://tomwritescode.com',
+    github: 'https://github.com/tomWritesCode',
+    twitter: 'https://twitter.com/tomWritesCode',
+    linkedin: 'https://www.linkedin.com/in/thomas-whitaker-b961b9112/',
+  },
 };
